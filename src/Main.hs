c1b32d4a101a34435c78feb8c7ddeda119b6a75a
MarkWRuszczycky <mwruszczycky@gmail.com>
2017-08-12 10:25:03 -0500

    Fixes up testing in Main.

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,26 @@ import qualified System.Environment as SE   ( getArgs )
 main :: IO ()
 main = do
     cmds <- SE.getArgs
+    let etOpts = S.parseCmdLine validOpts cmds
     case S.parseCmdLine validOpts cmds of
          Left  e -> putStrLn . show $ e
-         Right x -> prettyOptsArgs x
+         Right x -> doController x
 
-prettyOptsArgs :: S.OptsArgs -> IO ()
-prettyOptsArgs (opts, args) = do
-    mapM_ ( \ (o, a) -> putStrLn $ show o ++ " : " ++ show a ) opts
-    mapM_ ( putStrLn . show ) args
+doController :: S.OptsArgs -> IO ()
+doController (opts, args) = do
+    mapM_ ( prettyOpt opts ) validOpts
+    mapM_ ( \ x -> putStrLn $ "argument: " ++ x ) args
+
+prettyOpt :: S.Options -> S.Option -> IO ()
+prettyOpt opts opt = do
+    let check = show $ S.chkOpt opts opt
+        used  = show $ S.optUsed opts opt
+        disp  = show opt
+    putStrLn $ "option: " ++ disp ++ " | " ++ check ++ " | " ++ used
 
 validOpts :: S.ValidOptions
 validOpts = so ++ soa ++ lo ++ loa
     where so  = map S.Short    [ 'a', 'b', 'c' ]
-          soa = map S.ShortArg [ 'd', 'e', 'f' ]
+          soa = map S.ShortArg [ 'x', 'y', 'z' ]
           lo  = map S.Long     [ "runs", "flies", "swims" ]
           loa = map S.LongArg  [ "mammal", "fish", "bird" ]
